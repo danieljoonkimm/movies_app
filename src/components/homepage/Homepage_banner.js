000b4111d704b16b93f9989b5d8ab8b1f8629671
Daniel Kim <danieljoonkimm@gmail.com>
2020-10-12 22:33:25 -0700

    [edit] Editing optional chaining to prevent error

--- src/components/homepage/Homepage_banner.js
+++ src/components/homepage/Homepage_banner.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import axios from '../theme/axios'
+import request from '../theme/requests'
 
 export const HomepageBanner = () => {
   const [movie, setMovie] = useState([])
 
+  useEffect(() => {
+    const fetchData = async() => {
+      const {data} = await axios.get(request.fetchPopular)
+      setMovie(data.results[Math.floor(Math.random() * data.results.length - 1)])
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
-      hi
+      <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
     </div>
   )
 }
