69af2405f6fa6e1594e47aeae4c0cacde54c2017
Daniel Kim <danieljoonkimm@gmail.com>
2020-10-13 12:41:39 -0700

    [add] Creating styled-components based for reusability

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 import GlobalStyle from './components/theme/GlobalStyle'
 import { Homepage } from './components/homepage/Homepage'
 
+import { Card } from './components/Card'
+import { data } from './components/data'
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Homepage />
+      {/* <Homepage /> */}
+      {data.map(person => (
+        <Card>
+          <Card.Body>
+            <Card.Title>{person.first_name}</Card.Title>
+            <Card.Text>{person.gender}</Card.Text>
+            <Card.Button></Card.Button>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,41 @@
+export const data = [{
+  "id": 1,
+  "first_name": "Reube",
+  "gender": "Male"
+}, {
+  "id": 2,
+  "first_name": "Tootsie",
+  "gender": "Female"
+}, {
+  "id": 3,
+  "first_name": "Enos",
+  "gender": "Male"
+}, {
+  "id": 4,
+  "first_name": "Vic",
+  "gender": "Male"
+}, {
+  "id": 5,
+  "first_name": "Randie",
+  "gender": "Male"
+}, {
+  "id": 6,
+  "first_name": "Brice",
+  "gender": "Male"
+}, {
+  "id": 7,
+  "first_name": "Rich",
+  "gender": "Male"
+}, {
+  "id": 8,
+  "first_name": "Rodina",
+  "gender": "Female"
+}, {
+  "id": 9,
+  "first_name": "Ferdinand",
+  "gender": "Male"
+}, {
+  "id": 10,
+  "first_name": "Mela",
+  "gender": "Female"
+}]
