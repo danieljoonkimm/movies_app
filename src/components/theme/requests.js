031a98efe6481a82e8ccde0dc1ccde7dd57e4026
Daniel Kim <danieljoonkimm@gmail.com>
2020-10-12 20:49:14 -0700

    [edit] Editing top rated API fetch request

--- src/components/theme/requests.js
+++ src/components/theme/requests.js
@@ -1,7 +1,10 @@
 import { MOVIES_API } from './API_key.js'
 
-export default {
+const requests = {
   fetchPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIES_API}&language=en-US&page=1`,
   fetchLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${MOVIES_API}&language=en-US`,
-  fetchImages: `https://image.tmdb.org/t/p/w500/`
+  fetchImages: `https://image.tmdb.org/t/p/w500/`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${MOVIES_API}&language=en-US&page=1`
 }
+
+export default requests;
